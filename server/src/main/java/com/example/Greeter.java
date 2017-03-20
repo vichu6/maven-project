2a2cada60a036925ef20096242d4e5dcbce61fd1
Viswanathan Ramamurthy <viswanathan_ramamurthy@glic.com>
2017-03-20 13:11:28 -0400

    fixing warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,12 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+  *@param someone name of someone
+  *@return formatted greeting
+  */
+
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,5 @@
+/**
+*This is default package
+*
+*/
+package com.example;
